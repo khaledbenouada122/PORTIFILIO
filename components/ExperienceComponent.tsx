b7b614baf394d17639c3bbd78c27dc375456a7b3
unknown <khaled.ouada@khallasli.com>
2024-02-06 23:21:53 +0100

    some_update

--- components/ExperienceComponent.tsx
+++ components/ExperienceComponent.tsx
@@ -11,7 +11,7 @@ function ExperienceComponent({experiences}: Props) {
     <div className=' h-screen flex relative  flex-col text-left md:flex-row max-w-full px-10 justify-evenly  mx-auto items-center'>
         <h3 className='  absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
         <div className='flex justify-center'>
-    <div className='mt-10 md:mt-12 w-full flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/40'>
+    <div className='mt-10 md:mt-8 w-full flex space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/40'>
         {experiences?.map(experience => (
             <ExperienceCard key={experience._id} experience={experience} />
         ))}
