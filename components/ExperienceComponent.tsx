108de2774580248fb661a761785724894d138025
unknown <khaled.ouada@khallasli.com>
2024-02-07 22:44:27 +0100

    profile

--- components/ExperienceComponent.tsx
+++ components/ExperienceComponent.tsx
@@ -8,9 +8,9 @@ type Props = {
 
 function ExperienceComponent({experiences}: Props) {
   return (
-    <div className=' h-screen flex relative  flex-col text-left md:flex-row max-w-full px-10 justify-evenly  mx-auto items-center'>
+    <div className=' h-screen flex relative  flex-col text-left md:flex-row max-w-full px-10 py-30 justify-evenly  mx-auto items-center'>
         <h3 className='  absolute top-16 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
-        
+        <div className='flex '>
     <div className='mt-10 md:mt-8 w-full flex  justify-center space-x-5 overflow-x-scroll snap-x snap-mandatory scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#f7ab0a]/40'>
         {experiences?.map(experience => (
             <ExperienceCard key={experience._id} experience={experience} />
@@ -18,7 +18,7 @@ function ExperienceComponent({experiences}: Props) {
     </div>
 </div>
 
-    
+    </div>
   )
 }
 
